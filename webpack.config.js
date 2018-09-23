9c52e400f0014f2b24080f4ad3d8fab2c3dc594d
Brett <bretterjs@gmail.com>
2018-09-23 13:11:20 -0500

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require("path");
+
+module.exports = {
+
+  entry: {
+    main: ["./src/main.js"]
+  },
+
+  mode: "development",
+
+  output: {
+    filename: "[name]-bundle.js",
+    path: path.resolve(__dirname, "./dist"),
+    publicPath: "/"
+  },
+
+  devServer : {
+    contentBase: "dist"
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: "babel-loader"
+          }
+        ],
+        exclude: /node_modules/
+      },
+      {
+      test: /\.css$/,
+      use: [
+        {
+          loader: "style-loader",
+      },
+      {
+        loader: "css-loader"
+      }
+    ]
+  },
+    {
+      test: /\.html$/,
+      use: [
+        {
+          loader: "file-loader",
+          options: {
+            name: "[name].html"
+          }
+        },
+        {
+          loader: "extract-loader"
+        },
+        {
+          loader: "html-loader",
+          options: {
+            attrs:["img:src"]
+          }
+        }
+      ]
+    },
+    {
+      test: /\.(jpg|jpeg|gif|png|svg)$/,
+      use: [
+        {
+          loader: "file-loader",
+          options: {
+            name: "images/[name].[ext]"
+          }
+        }
+      ]
+    }
+    ]
+  }
+}
+
